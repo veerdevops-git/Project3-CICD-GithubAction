fa6a67eac6515dc0eb7bb8d5d171d66db522e06a
veeranna <veeranna.git@abc.com>
2025-05-04 19:29:58 +0530

    testing after ArgoCd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from GitHub Actions! by mobile'));
+app.get('/', (req, res) => res.send('Hello from GitHub Actions! Thank you installed ArgoCD GitOps'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
